01fc6faa13d5853af3b6d83ea68aa5faac2880ea
Maanav Khaitan <maanav@berkeley.edu>
2024-07-05 19:39:54 -0400

    Add basic Consumer.sol

---
+++ contracts/src/Consumer.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+pragma solidity ^0.8.13;
+import {JobManager} from "./JobManager.sol";
+
+abstract contract Consumer {
+    mapping(uint32 => bytes) internal jobIDToInputs;
+    JobManager internal _jobManager;
+
+    constructor(address jobManager) {
+        _jobManager = JobManager(jobManager);
+    }
+
+    modifier onlyJobManager() {
+        require(
+            msg.sender == address(_jobManager),
+            "Consumer.onlyJobManager: caller is not the job manager"
+        );
+        _;
+    }
+
+    function requestJob(
+        bytes calldata programID,
+        bytes calldata inputs
+    ) public returns (uint32) {
+        uint32 jobID = _jobManager.createJob(programID, inputs);
+        jobIDToInputs[jobID] = inputs;
+        return jobID;
+    }
+
+    function getJobInputs(uint32 jobID) public view returns (bytes memory) {
+        return jobIDToInputs[jobID];
+    }
+
+    function receiveResult(uint32 jobID, bytes calldata result) public onlyJobManager {
+        // Decode the coprocessor result into app-specific struct
+
+        // Perform app-specific logic using the result
+    }
+}
