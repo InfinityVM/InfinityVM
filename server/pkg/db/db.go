ca52f5517bb08a20add9f8c46f541f2ba3513033
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2024-07-08 11:11:49 -0700

    updates

--- server/pkg/db/db.go
+++ server/pkg/db/db.go
@@ -2,8 +2,12 @@ package db
 
 // DB defines an interface for key/value retrieval and storage.
 type DB interface {
+	// Set stores the key/value pair in the database.
 	Set([]byte, []byte) error
+	// Get retrieves the value for the given key.
 	Get([]byte) ([]byte, error)
+	// Delete removes the key/value pair from the database.
 	Delete([]byte) error
+	// Close closes the database connection.
 	Close() error
 }
