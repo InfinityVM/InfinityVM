c3b76f7eab2b334dfd9309f8dfa08c3ab84812ef
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2024-07-08 09:12:26 -0700

    updates

--- server/pkg/queue/queue.go
+++ server/pkg/queue/queue.go
@@ -1,1 +1,19 @@
 package queue
+
+import (
+	"github.com/ethos-works/InfinityVM/server/pkg/types"
+)
+
+// Queue defines the interface for a queue that can be used to produce jobs.
+type Queue interface {
+	// Push adds a job to the queue.
+	Push(j types.Job) error
+	// Pop removes a job from the queue.
+	Pop() (types.Job, error)
+	// Peek returns the next job in the queue without removing it.
+	Peek() (types.Job, error)
+	// Reset clears the queue.
+	Reset() error
+	// Size returns the number of jobs in the queue.
+	Size() int
+}
