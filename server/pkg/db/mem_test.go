ff8671cbb2ce5ceadc20f07a249af2f6400ebf20
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2024-07-08 11:09:34 -0700

    updates

--- server/pkg/db/mem_test.go
+++ server/pkg/db/mem_test.go
@@ -1,6 +1,7 @@
 package db_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -15,4 +16,19 @@ func TestMemDB(t *testing.T) {
 	defer func() {
 		require.NoError(t, db.Close())
 	}()
+
+	err = db.Delete([]byte("key001"))
+	require.NoError(t, err)
+
+	for i := 0; i < 100; i++ {
+		key := []byte(fmt.Sprintf("key%03d", i))
+		val := []byte(fmt.Sprintf("val%03d", i))
+
+		err = db.Set(key, val)
+		require.NoError(t, err)
+
+		result, err := db.Get(key)
+		require.NoError(t, err)
+		require.Equal(t, val, result)
+	}
 }
