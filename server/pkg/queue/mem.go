6f1dd7b34def62f29c71d7fef139fed423856867
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2024-07-08 09:41:34 -0700

    updates

--- server/pkg/queue/mem.go
+++ server/pkg/queue/mem.go
@@ -2,10 +2,13 @@ package queue
 
 import (
 	"container/list"
+	"sync"
 )
 
-// MemQueue is an in-memory implementation of the Queue interface using FIFO order.
+// MemQueue is an in-memory thread-safe implementation of the Queue interface
+// using FIFO order.
 type MemQueue[T any] struct {
+	mu        sync.RWMutex
 	container *list.List
 }
 
@@ -16,11 +19,17 @@ func NewMemQueue[T any]() Queue[T] {
 }
 
 func (m *MemQueue[T]) Push(x T) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
 	m.container.PushBack(x)
 	return nil
 }
 
 func (m *MemQueue[T]) Pop() (T, error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
 	var result T
 
 	x := m.container.Front()
@@ -35,6 +44,9 @@ func (m *MemQueue[T]) Pop() (T, error) {
 }
 
 func (m *MemQueue[T]) Peek() (T, error) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
 	var result T
 
 	x := m.container.Front()
@@ -47,10 +59,16 @@ func (m *MemQueue[T]) Peek() (T, error) {
 }
 
 func (m *MemQueue[T]) Reset() error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
 	m.container = m.container.Init()
 	return nil
 }
 
 func (m *MemQueue[T]) Size() int {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
 	return m.container.Len()
 }
