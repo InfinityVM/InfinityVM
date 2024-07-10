e75e9db45ecc3602baf40d470669659535b932b9
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2024-07-10 10:58:04 -0700

    updates

--- server/pkg/server/grpc.go
+++ server/pkg/server/grpc.go
@@ -3,6 +3,7 @@ package server
 import (
 	"context"
 
+	"github.com/ethos-works/InfinityVM/server/pkg/executor"
 	"github.com/ethos-works/InfinityVM/server/pkg/types"
 )
 
@@ -10,10 +11,14 @@ var _ types.ServiceServer = &Server{}
 
 type Server struct {
 	types.UnimplementedServiceServer
+
+	executor *executor.Executor
 }
 
-func New() *Server {
-	return &Server{}
+func New(e *executor.Executor) *Server {
+	return &Server{
+		executor: e,
+	}
 }
 
 func (s *Server) SubmitJob(context.Context, *types.SubmitJobRequest) (*types.SubmitJobResponse, error) {
@@ -24,6 +29,6 @@ func (s *Server) GetResult(context.Context, *types.GetResultRequest) (*types.Get
 	panic("not implemented!")
 }
 
-func (s *Server) CreateProgram(context.Context, *types.CreateProgramRequest) (*types.CreateProgramResponse, error) {
+func (s *Server) SubmitProgram(context.Context, *types.SubmitProgramRequest) (*types.SubmitProgramResponse, error) {
 	panic("not implemented!")
 }
