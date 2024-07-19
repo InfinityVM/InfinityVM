e3ec1b0a89240c77ac41ccc733bab515dc12604c
Maanav Khaitan <maanav@berkeley.edu>
2024-07-19 15:46:11 -0400

    Added log level

--- rust/crates/cmd/src/main.rs
+++ rust/crates/cmd/src/main.rs
@@ -1,9 +1,11 @@
 use clap::{Arg, ArgMatches, Command};
+use tracing_subscriber::{fmt, filter::EnvFilter, prelude::*};
 use std::error::Error;
 use std::str::FromStr;
 use tonic::transport::Server;
 use tracing::Level;
 use tracing_subscriber::fmt::Subscriber as FmtSubscriber;
+use alloy::primitives::Address;
 
 // CLI flag and value constants
 const LOG_LEVEL_JSON: &str = "json";
@@ -71,17 +73,41 @@ async fn main() -> Result<(), Box<dyn Error>> {
         )
         .get_matches();
 
+    let log_level: &String = matches.get_one(FLAG_LOG_LEVEL).unwrap();
+    let log_format: &String = matches.get_one(FLAG_LOG_FORMAT).unwrap();
+
+    let filter = EnvFilter::try_new(log_level)?;
+    let subscriber: Box<dyn tracing::Subscriber + Send + Sync> = match log_format.as_str() {
+        LOG_LEVEL_JSON => Box::new(FmtSubscriber::builder()
+            .json()
+            .with_env_filter(filter)
+            .finish()),
+        LOG_LEVEL_TEXT => Box::new(FmtSubscriber::builder()
+            .with_env_filter(filter)
+            .finish()),
+        _ => return Err(format!("Invalid logging format: {}", log_format).into()),
+    };
+    tracing::subscriber::set_global_default(subscriber)?;
+
     let grpc_address: &String = matches.get_one(FLAG_GRPC_ADDRESS).unwrap();
     let grpc_gateway_address: &String = matches.get_one(FLAG_GRPC_GATEWAY_ADDRESS).unwrap();
     let zk_shim_address: &String = matches.get_one(FLAG_ZK_SHIM_ADDRESS).unwrap();
-    let job_manager_address: &String = matches.get_one(FLAG_JOB_MANAGER_ADDRESS).unwrap();
+
+    let job_manager_address_string: &String = matches.get_one(FLAG_JOB_MANAGER_ADDRESS).unwrap();
+    let job_manager_address = job_manager_address_string.parse::<Address>()
+    .map_err(|_| format!("Invalid Ethereum address: {}", job_manager_address_string))?;
+
     let eth_rpc_address: &String = matches.get_one(FLAG_ETH_RPC_ADDRESS).unwrap();
+
+    let relayer_private_key = std::env::var(ENV_RELAYER_PRIV_KEY)
+    .map_err(|_| format!("Environment variable {} must be set", ENV_RELAYER_PRIV_KEY))?;
 
     println!("Starting gRPC server at: {}", grpc_address);
     println!("Starting gRPC gateway server at: {}", grpc_gateway_address);
     println!("ZK shim address: {}", zk_shim_address);
     println!("JobManager contract address: {}", job_manager_address);
     println!("Ethereum RPC address: {}", eth_rpc_address);
+    println!("Relayer private key: {}", relayer_private_key);
 
     // You would set up your gRPC server and other components here.
     // For example:
