e897fe9df1c6bef89f5b8a63850d677cd46adb38
Zeke Mostov <zeke@turnkey.io>
2024-07-19 21:49:51 -0400

    make protos compile

--- rust/crates/proto/build.rs
+++ rust/crates/proto/build.rs
@@ -8,7 +8,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     tonic_build::configure()
         .file_descriptor_set_path(out_dir.join("zkvm_executor_descriptor.bin"))
-        .compile(&["../../../proto/server/v1/zkvm_executor.proto"], &["../../../proto/server/v1"])
+        .compile(
+            &[
+                "../../../proto/server/v1/zkvm_executor.proto",
+                "../../../proto/server/v1/service.proto",
+                "../../../proto/server/v1/job.proto",
+            ],
+            &["../../../proto"],
+        )
         .unwrap();
 
     Ok(())
