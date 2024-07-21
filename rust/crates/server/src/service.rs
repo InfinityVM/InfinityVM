cd63c1f9167473c140c779607556a3d047a84996
Maanav Khaitan <maanav@berkeley.edu>
2024-07-21 16:23:17 -0400

    Added parameter checks to grpc logic

--- rust/crates/server/src/service.rs
+++ rust/crates/server/src/service.rs
@@ -1,6 +1,8 @@
 use proto::{
-    GetResultRequest, GetResultResponse, SubmitJobRequest, SubmitJobResponse, SubmitProgramRequest,
-    SubmitProgramResponse,
+    GetResultRequest, GetResultResponse, Job, SubmitJobRequest, SubmitJobResponse, SubmitProgramRequest, SubmitProgramResponse
+};
+use tonic::{
+    Request, Response, Status
 };
 
 /// Use me!
@@ -19,11 +21,32 @@ impl proto::service_server::Service for Server {
     /// SubmitJob defines the gRPC method for submitting a coprocessing job.
     async fn submit_job(
         &self,
-        _request: tonic::Request<SubmitJobRequest>,
-    ) -> std::result::Result<tonic::Response<SubmitJobResponse>, tonic::Status> {
-        println!("submit_job");
-        Ok(tonic::Response::new(SubmitJobResponse {
-            job_id: 0,
+        request: Request<SubmitJobRequest>,
+    ) -> Result<Response<SubmitJobResponse>, Status> {
+        let job = request.into_inner().job.ok_or_else(|| Status::invalid_argument("empty request"))?;
+
+        // verify fields
+        if job.max_cycles == 0 {
+            return Err(tonic::Status::invalid_argument("job max cycles must be positive"));
+        }
+
+        if job.id == 0 {
+            return Err(tonic::Status::invalid_argument("job ID must be positive"));
+        }
+
+        if job.contract_address.is_empty() {
+            return Err(tonic::Status::invalid_argument("job contract address must not be empty"));
+        }
+
+        if job.program_verifying_key.is_empty() {
+            return Err(tonic::Status::invalid_argument("job program verification key must not be empty"));
+        }
+
+        // TODO: Implement executor in Rust
+        // executor.submit_job(job)
+
+        Ok(Response::new(SubmitJobResponse {
+            job_id: job.id,
         }))
     }
     /// GetResult defines the gRPC method for getting the result of a coprocessing
@@ -32,7 +55,14 @@ impl proto::service_server::Service for Server {
         &self,
         _request: tonic::Request<GetResultRequest>,
     ) -> std::result::Result<tonic::Response<GetResultResponse>, tonic::Status> {
-        println!("get_result");
+        let req = _request.into_inner();
+        if req.job_id == 0 {
+            return Err(tonic::Status::invalid_argument("job ID must be positive"));
+        }
+
+        // TODO: Implement executor in Rust
+        // let job = executor.get_job(job_id)
+
         Ok(tonic::Response::new(GetResultResponse {
             job: None,
         }))
@@ -43,7 +73,14 @@ impl proto::service_server::Service for Server {
         &self,
         _request: tonic::Request<SubmitProgramRequest>,
     ) -> std::result::Result<tonic::Response<SubmitProgramResponse>, tonic::Status> {
-        println!("submit_program");
+        let req = _request.into_inner();
+        if req.program_elf.is_empty() {
+            return Err(tonic::Status::invalid_argument("program elf must not be empty"));
+        }
+        
+        // TODO: Implement executor in Rust
+        // let verifying_key = executor.submit_elf(req.program_elf, req.vm_type);
+
         Ok(tonic::Response::new(SubmitProgramResponse {
             program_verifying_key: vec![],
         }))
