aef82ff8d24991b70c32ff18339f958c216c8ba9
Maanav Khaitan <maanav@berkeley.edu>
2024-07-22 18:14:33 -0400

    clean build.rs

--- rust/crates/proto/build.rs
+++ rust/crates/proto/build.rs
@@ -7,31 +7,7 @@ use std::{env, path::PathBuf};
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     tonic_build::configure()
-        .type_attribute("server.v1.Job", "#[derive(serde::Serialize, serde::Deserialize)]")
-        .type_attribute(
-            "server.v1.SubmitJobRequest",
-            "#[derive(serde::Serialize, serde::Deserialize)]",
-        )
-        .type_attribute(
-            "server.v1.SubmitJobResponse",
-            "#[derive(serde::Serialize, serde::Deserialize)]",
-        )
-        .type_attribute(
-            "server.v1.GetResultRequest",
-            "#[derive(serde::Serialize, serde::Deserialize)]",
-        )
-        .type_attribute(
-            "server.v1.GetResultResponse",
-            "#[derive(serde::Serialize, serde::Deserialize)]",
-        )
-        .type_attribute(
-            "server.v1.SubmitProgramRequest",
-            "#[derive(serde::Serialize, serde::Deserialize)]",
-        )
-        .type_attribute(
-            "server.v1.SubmitProgramResponse",
-            "#[derive(serde::Serialize, serde::Deserialize)]",
-        )
+        .type_attribute(".server", "#[derive(serde::Serialize, serde::Deserialize)]")
         .file_descriptor_set_path(out_dir.join("descriptor.bin"))
         .compile(
             &[
