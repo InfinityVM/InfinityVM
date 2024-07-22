6f4f85d004337ffb1408c878cff1cac5a076facb
Maanav Khaitan <maanav@berkeley.edu>
2024-07-22 11:45:06 -0400

    clean up tonic::

--- rust/crates/server/src/service.rs
+++ rust/crates/server/src/service.rs
@@ -33,19 +33,19 @@ impl proto::service_server::Service for Server {
 
         // verify fields
         if job.max_cycles == 0 {
-            return Err(tonic::Status::invalid_argument("job max cycles must be positive"));
+            return Err(Status::invalid_argument("job max cycles must be positive"));
         }
 
         if job.id == 0 {
-            return Err(tonic::Status::invalid_argument("job ID must be positive"));
+            return Err(Status::invalid_argument("job ID must be positive"));
         }
 
         if job.contract_address.is_empty() {
-            return Err(tonic::Status::invalid_argument("job contract address must not be empty"));
+            return Err(Status::invalid_argument("job contract address must not be empty"));
         }
 
         if job.program_verifying_key.is_empty() {
-            return Err(tonic::Status::invalid_argument(
+            return Err(Status::invalid_argument(
                 "job program verification key must not be empty",
             ));
         }
@@ -59,32 +59,32 @@ impl proto::service_server::Service for Server {
     /// job.
     async fn get_result(
         &self,
-        request: tonic::Request<GetResultRequest>,
-    ) -> std::result::Result<tonic::Response<GetResultResponse>, tonic::Status> {
+        request: Request<GetResultRequest>,
+    ) -> std::result::Result<Response<GetResultResponse>, Status> {
         let req = request.into_inner();
         if req.job_id == 0 {
-            return Err(tonic::Status::invalid_argument("job ID must be positive"));
+            return Err(Status::invalid_argument("job ID must be positive"));
         }
 
         // TODO: Implement executor in Rust
         // let job = executor.get_job(job_id)
 
-        Ok(tonic::Response::new(GetResultResponse { job: None }))
+        Ok(Response::new(GetResultResponse { job: None }))
     }
     /// SubmitProgram defines the gRPC method for submitting a new program to
     /// generate a unique program verification key.
     async fn submit_program(
         &self,
-        request: tonic::Request<SubmitProgramRequest>,
-    ) -> std::result::Result<tonic::Response<SubmitProgramResponse>, tonic::Status> {
+        request: Request<SubmitProgramRequest>,
+    ) -> std::result::Result<Response<SubmitProgramResponse>, Status> {
         let req = request.into_inner();
         if req.program_elf.is_empty() {
-            return Err(tonic::Status::invalid_argument("program elf must not be empty"));
+            return Err(Status::invalid_argument("program elf must not be empty"));
         }
 
         // TODO: Implement executor in Rust
         // let verifying_key = executor.submit_elf(req.program_elf, req.vm_type);
 
-        Ok(tonic::Response::new(SubmitProgramResponse { program_verifying_key: vec![] }))
+        Ok(Response::new(SubmitProgramResponse { program_verifying_key: vec![] }))
     }
 }
