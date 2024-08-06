e8108be9bd1e2bb96b04f942951b5499d1a71417
Zeke Mostov <zeke@ethosstake.com>
2024-08-06 11:44:15 -0400

    Update rust/crates/coprocessor-node/src/cli.rs
    Co-authored-by: lucasege <lucas@ege.com>

--- rust/crates/coprocessor-node/src/cli.rs
+++ rust/crates/coprocessor-node/src/cli.rs
@@ -139,7 +139,7 @@ struct Opts {
     #[arg(long, default_value = "http://127.0.0.1:8545")]
     http_eth_rpc: String,
 
-    /// WS Ethereum RPC address. Defaults to anvil node default address.
+    /// WS Ethereum RPC address. Defaults to a local anvil node address.
     #[arg(long, default_value = "ws://127.0.0.1:8545")]
     ws_eth_rpc: String,
 
