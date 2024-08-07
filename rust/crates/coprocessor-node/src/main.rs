8597a792219d3afdaf208069adb4a33dccaa6457
Aniket Dixit <dixitaniket199@gmail.com>
2024-08-07 13:16:37 +0530

    logging to stdout

--- rust/crates/coprocessor-node/src/main.rs
+++ rust/crates/coprocessor-node/src/main.rs
@@ -4,7 +4,10 @@ use coprocessor_node::cli::Cli;
 
 #[tokio::main]
 async fn main() -> Result<(), impl std::error::Error> {
+    let (non_blocking, _guard) = tracing_appender::non_blocking(std::io::stdout());
     tracing_subscriber::fmt()
+        .json()
+        .with_writer(non_blocking)
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .compact()
         .init();
