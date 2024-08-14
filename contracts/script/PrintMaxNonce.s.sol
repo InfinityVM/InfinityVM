79fde2fe8bddbdb2c551aa94831f7e9a0e0399d1
Maanav Khaitan <maanav@berkeley.edu>
2024-08-13 21:30:59 -0400

    Added CLI command to print max nonce

---
+++ contracts/script/PrintMaxNonce.s.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {Utils} from "./utils/Utils.sol";
+import {MockConsumer} from "../test/mocks/MockConsumer.sol";
+import "forge-std/StdJson.sol";
+
+contract PrintMaxNonce is Script, Utils {
+
+    MockConsumer public consumer;
+
+    function printMaxNonce() public {
+        string memory coprocessorDeployedContracts = readOutput(
+            "coprocessor_deployment_output"
+        );
+
+        consumer = MockConsumer(
+            stdJson.readAddress(
+                coprocessorDeployedContracts,
+                ".addresses.consumer"
+            )
+        );
+
+        uint32 maxNonce = consumer.getMaxNonce();
+        console.log("Max nonce: ", maxNonce);
+    }
+
+}
