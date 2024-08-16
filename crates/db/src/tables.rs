85128505157c9e76cf5f4cb434ff7751b670d79c
Zeke Mostov <zeke@turnkey.io>
2024-08-16 11:30:06 -0400

    chore: use macro for compress/decompress derives

--- crates/db/src/tables.rs
+++ crates/db/src/tables.rs
@@ -4,7 +4,7 @@ use alloy::rlp::bytes;
 use borsh::{BorshDeserialize, BorshSerialize};
 use proto::Job;
 use reth_db::{
-    table::{Compress, Decode, Decompress, Encode},
+    table::{Decode, Encode},
     tables, DatabaseError, TableType, TableViewer,
 };
 use sha2::{Digest, Sha256};
@@ -68,19 +68,26 @@ pub struct ElfWithMeta {
     pub elf: Vec<u8>,
 }
 
-impl Compress for ElfWithMeta {
-    type Compressed = Vec<u8>;
-    fn compress_to_buf<B: bytes::BufMut + AsMut<[u8]>>(self, dest: &mut B) {
-        let src = borsh::to_vec(&self).expect("borsh serialize works. qed.");
-        dest.put(&src[..])
-    }
+macro_rules! impl_compress_decompress {
+    ($name:ident) => {
+        impl reth_db::table::Compress for $name {
+            type Compressed = Vec<u8>;
+
+            fn compress_to_buf<B: bytes::buf::BufMut + AsMut<[u8]>>(self, dest: &mut B) {
+                let src = borsh::to_vec(&self).expect("borsh serialize works. qed.");
+                dest.put(&src[..])
+            }
+        }
+
+        impl reth_db::table::Decompress for $name {
+            fn decompress<B: AsRef<[u8]>>(value: B) -> Result<Self, reth_db::DatabaseError> {
+                borsh::from_slice(value.as_ref()).map_err(|_| reth_db::DatabaseError::Decode)
+            }
+        }
+    };
 }
 
-impl Decompress for ElfWithMeta {
-    fn decompress<B: AsRef<[u8]>>(value: B) -> Result<Self, DatabaseError> {
-        borsh::from_slice(value.as_ref()).map_err(|_| DatabaseError::Decode)
-    }
-}
+impl_compress_decompress! { ElfWithMeta }
 
 reth_db::tables! {
     /// Stores Elf files
