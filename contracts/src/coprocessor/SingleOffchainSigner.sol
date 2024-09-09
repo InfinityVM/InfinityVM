f48c84e307abc2d34fafc7b6e7da7be7c7313778
Maanav Khaitan <maanav@berkeley.edu>
2024-09-09 17:10:15 -0400

    refactor: Add interface to generalize offchain signer

---
+++ contracts/src/coprocessor/SingleOffchainSigner.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {OffchainRequester} from "./OffchainRequester.sol";
+import {ECDSA} from "solady/utils/ECDSA.sol";
+
+abstract contract SingleOffchainSigner is OffchainRequester {
+    // SingleOffchainSigner allows a single offchainSigner address to sign all offchain job requests
+    address private offchainSigner;
+
+    constructor(address _offchainSigner) OffchainRequester() {
+        offchainSigner = _offchainSigner;
+    }
+
+    function getOffchainSigner() external view returns (address) {
+        return offchainSigner;
+    }
+
+    // Included for EIP-1271. The JobManager calls this function to verify the signature on
+    // an offchain job request.
+    function isValidSignature(bytes32 messageHash, bytes memory signature) public view override returns (bytes4) {
+        address recoveredSigner = ECDSA.tryRecover(messageHash, signature);
+        // ClobConsumer allows a single offchainSigner address to sign all offchain job requests
+        if (recoveredSigner == offchainSigner) {
+            return EIP1271_MAGIC_VALUE;
+        } else {
+            return INVALID_SIGNATURE;
+        }
+    }
+}
