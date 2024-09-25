e926f66e25ecf881a6b9e1394828390dc4c8bcb5
Maanav Khaitan <maanav@berkeley.edu>
2024-09-25 19:21:39 -0400

    feat: Add script to quickly test devnet setup and clean up load tests (#235)

---
+++ crates/scripts/src/node_test.rs
@@ -0,0 +1,147 @@
+//! Test the coprocessor node by submitting programs, submitting a job, and getting the result.
+
+use alloy::{
+    primitives::{hex, Address, FixedBytes},
+    providers::ProviderBuilder,
+    signers::local::LocalSigner,
+    sol_types::SolValue,
+};
+use clob_programs::CLOB_ELF;
+use contracts::mock_consumer::MockConsumer;
+use k256::ecdsa::SigningKey;
+use mock_consumer::MOCK_CONSUMER_MAX_CYCLES;
+use mock_consumer_methods::{MOCK_CONSUMER_GUEST_ELF, MOCK_CONSUMER_GUEST_ID};
+use proto::{
+    coprocessor_node_client::CoprocessorNodeClient, GetResultRequest, GetResultResponse,
+    SubmitJobRequest, SubmitProgramRequest, VmType,
+};
+use test_utils::create_and_sign_offchain_request;
+use tracing::{error, info};
+
+const COPROCESSOR_IP: &str = "34.82.138.182";
+const COPROCESSOR_GRPC_PORT: u16 = 50420;
+const COPROCESSOR_HTTP_PORT: u16 = 8080;
+const MOCK_CONSUMER_ADDR: &str = "0x0165878A594ca255338adfa4d48449f69242Eb8F";
+const OFFCHAIN_SIGNER_PRIVATE_KEY: &str =
+    "0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6";
+const ANVIL_IP: &str = "35.230.81.89";
+const ANVIL_PORT: u16 = 8545;
+
+type K256LocalSigner = LocalSigner<SigningKey>;
+
+#[tokio::main]
+async fn main() {
+    let subscriber = tracing_subscriber::FmtSubscriber::new();
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+
+    let client_coproc_grpc = format!("http://{COPROCESSOR_IP}:{COPROCESSOR_GRPC_PORT}");
+    let mut coproc_client =
+        CoprocessorNodeClient::connect(client_coproc_grpc.clone()).await.unwrap();
+
+    info!("Trying to submit CLOB ELF to coprocessor node");
+    let submit_program_request =
+        SubmitProgramRequest { program_elf: CLOB_ELF.to_vec(), vm_type: VmType::Risc0.into() };
+    // We handle error here because we don't want to panic if the ELF was already submitted
+    match coproc_client.submit_program(submit_program_request).await {
+        Ok(submit_program_response) => {
+            info!("Submitted CLOB ELF to coprocessor node: {:?}", submit_program_response);
+        }
+        Err(e) => {
+            error!("Error while submitting CLOB ELF to coprocessor node: {:?}", e);
+        }
+    }
+
+    info!("Trying to submit MockConsumer ELF to coprocessor node");
+    let submit_program_request = SubmitProgramRequest {
+        program_elf: MOCK_CONSUMER_GUEST_ELF.to_vec(),
+        vm_type: VmType::Risc0.into(),
+    };
+    // We handle error here because we don't want to panic if the ELF was already submitted
+    match coproc_client.submit_program(submit_program_request).await {
+        Ok(submit_program_response) => {
+            info!("Submitted MockConsumer ELF to coprocessor node: {:?}", submit_program_response);
+        }
+        Err(e) => {
+            error!("Error while submitting MockConsumer ELF to coprocessor node: {:?}", e);
+        }
+    }
+
+    // Get nonce from the consumer contract
+    let provider = ProviderBuilder::new().with_recommended_fillers().on_http(
+        url::Url::parse(format!("http://{ANVIL_IP}:{ANVIL_PORT}").as_str()).expect("Valid URL"),
+    );
+    let consumer_addr =
+        Address::parse_checksummed(MOCK_CONSUMER_ADDR, None).expect("Valid address");
+    let consumer_contract = MockConsumer::new(consumer_addr, &provider);
+    let nonce = consumer_contract.getNextNonce().call().await.unwrap()._0;
+
+    let decoded = hex::decode(OFFCHAIN_SIGNER_PRIVATE_KEY).unwrap();
+    let offchain_signer = K256LocalSigner::from_slice(&decoded).unwrap();
+
+    let (encoded_job_request, signature) = create_and_sign_offchain_request(
+        nonce,
+        MOCK_CONSUMER_MAX_CYCLES,
+        consumer_addr,
+        Address::abi_encode(&consumer_addr).as_slice(),
+        &MOCK_CONSUMER_GUEST_ID.iter().flat_map(|&x| x.to_le_bytes()).collect::<Vec<u8>>(),
+        offchain_signer.clone(),
+        &[],
+        &[],
+    )
+    .await;
+
+    // Submit a job to the coprocessor node
+    let submit_job_request = SubmitJobRequest {
+        request: encoded_job_request,
+        signature,
+        offchain_input: Vec::new(),
+        state: Vec::new(),
+    };
+
+    let submit_job_response = coproc_client.submit_job(submit_job_request).await.unwrap();
+    info!("Submitted job: {:?}", submit_job_response);
+
+    tokio::time::sleep(tokio::time::Duration::from_secs(4)).await;
+
+    // Get the job result from the coprocessor node
+    let job_id = submit_job_response.into_inner().job_id;
+    let get_result_request = GetResultRequest { job_id: job_id.clone() };
+    let get_result_response = coproc_client.get_result(get_result_request).await.unwrap();
+    info!("Result: {:?}", get_result_response);
+
+    // Call get_result on HTTP gateway as well to make sure the gateway works correctly
+    let http_gateway_url = format!("http://{COPROCESSOR_IP}:{COPROCESSOR_HTTP_PORT}");
+    let client = reqwest::Client::new();
+
+    let get_result_request = GetResultRequest { job_id: job_id.to_vec() };
+    let response = client
+        .post(format!("{}/v1/coprocessor_node/get_result", http_gateway_url))
+        .json(&get_result_request)
+        .send()
+        .await
+        .expect("Failed to send request");
+
+    let get_result_response: GetResultResponse =
+        response.json().await.expect("Failed to parse JSON response");
+    info!("HTTP Gateway Result: {:?}", get_result_response);
+
+    // Wait for the job result to be relayed to anvil
+    let mut inputs = vec![0u8; 32];
+    // If the inputs are all zero, then the job is not yet relayed
+    while inputs.iter().all(|&x| x == 0) {
+        let fixed_size_job_id: [u8; 32] = job_id.as_slice().try_into().expect("Fixed size array");
+        let get_inputs_call =
+            consumer_contract.getOnchainInputForJob(FixedBytes(fixed_size_job_id));
+        match get_inputs_call.call().await {
+            Ok(MockConsumer::getOnchainInputForJobReturn { _0: result }) => {
+                inputs = result.to_vec();
+                if inputs.iter().all(|&x| x == 0) {
+                    tokio::time::sleep(tokio::time::Duration::from_millis(10)).await;
+                }
+            }
+            Err(e) => eprintln!("Error calling getOnchainInputForJob: {:?}", e),
+        }
+    }
+
+    info!("Job result relayed to anvil with inputs: {:?}", inputs);
+}
--- test-load/src/main.rs
+++ test-load/src/main.rs
@@ -1,23 +1,19 @@
 //! Load testing for the coprocessor node
-use abi::{abi_encode_offchain_job_request, JobParams};
-use alloy::{
-    primitives::{keccak256, Address},
-    providers::ProviderBuilder,
-    signers::Signer,
-    sol_types::SolValue,
-};
-use contracts::{get_default_deploy_info, mock_consumer::MockConsumer};
+use alloy::{primitives::Address, providers::ProviderBuilder};
+use contracts::mock_consumer::MockConsumer;
 use db::tables::get_job_id;
 use goose::prelude::*;
-use mock_consumer_methods::MOCK_CONSUMER_GUEST_ID;
 use once_cell::sync::Lazy;
-use proto::{GetResultRequest, GetResultResponse, SubmitJobRequest};
+use proto::{GetResultRequest, SubmitJobRequest};
 use std::{
-    env,
     sync::atomic::{AtomicU64, Ordering},
     time::{Duration, Instant},
 };
-use test_utils::get_signers;
+use test_load::{
+    anvil_ip, anvil_port, consumer_addr, coprocessor_gateway_ip, coprocessor_gateway_port,
+    get_offchain_request, num_users, report_file_name, run_time, should_wait_until_job_completed,
+    startup_time, wait_until_job_completed,
+};
 use tokio::sync::OnceCell;
 
 static GLOBAL_NONCE: Lazy<OnceCell<AtomicU64>> = Lazy::new(OnceCell::new);
@@ -35,70 +31,6 @@ async fn initialize_global_nonce() -> AtomicU64 {
     let nonce = consumer_contract.getNextNonce().call().await.unwrap()._0;
 
     AtomicU64::new(nonce)
-}
-
-fn anvil_ip() -> String {
-    env::var("ANVIL_IP").unwrap_or_else(|_| "127.0.0.1".to_string())
-}
-
-fn anvil_port() -> String {
-    env::var("ANVIL_PORT").unwrap_or_else(|_| "8545".to_string())
-}
-
-fn coprocessor_gateway_ip() -> String {
-    env::var("COPROCESSOR_GATEWAY_IP").unwrap_or_else(|_| "127.0.0.1".to_string())
-}
-
-fn coprocessor_gateway_port() -> String {
-    env::var("COPROCESSOR_GATEWAY_PORT").unwrap_or_else(|_| "8080".to_string())
-}
-
-fn max_cycles() -> u64 {
-    match env::var("MAX_CYCLES") {
-        Ok(max_cycles) => max_cycles.parse().unwrap_or(1000000),
-        Err(_) => 1000000, // Default value if MAX_CYCLES is not set
-    }
-}
-
-fn consumer_addr() -> String {
-    match env::var("CONSUMER_ADDR") {
-        Ok(consumer_addr) => consumer_addr,
-        Err(_) => {
-            // If env var is not set, try to get the consumer address from the deploy info
-            let deploy_info = get_default_deploy_info();
-            match deploy_info {
-                Ok(deploy_info) => deploy_info.mock_consumer.to_string(),
-                Err(_) => "0xbdEd0D2bf404bdcBa897a74E6657f1f12e5C6fb6".to_string(), /* Default consumer address */
-            }
-        }
-    }
-}
-
-fn should_wait_until_job_completed() -> bool {
-    match env::var("WAIT_UNTIL_JOB_COMPLETED") {
-        Ok(enabled) => enabled.to_lowercase() == "true",
-        Err(_) => false,
-    }
-}
-
-fn num_users() -> usize {
-    env::var("NUM_USERS").ok().and_then(|v| v.parse().ok()).unwrap_or(10) // Default to 10 users if
-                                                                          // not set or invalid
-}
-
-fn report_file_name() -> String {
-    env::var("REPORT_FILE_NAME").ok().unwrap_or_else(|| "report.html".to_string())
-}
-
-fn startup_time() -> usize {
-    env::var("STARTUP_TIME").ok().and_then(|v| v.parse().ok()).unwrap_or(10) // Default to 10
-                                                                             // seconds if not set
-                                                                             // or invalid
-}
-
-fn run_time() -> usize {
-    env::var("RUN_TIME").ok().and_then(|v| v.parse().ok()).unwrap_or(20) // Default to 20 seconds if
-                                                                         // not set or invalid
 }
 
 #[tokio::main]
@@ -134,44 +66,13 @@ async fn main() -> Result<(), GooseError> {
     Ok(())
 }
 
-async fn submit_first_job() -> Result<(), Box<dyn std::error::Error>> {
-    let nonce = GLOBAL_NONCE.get().unwrap().fetch_add(1, Ordering::SeqCst);
-
-    if nonce == 1 {
-        let (encoded_job_request, signature) = create_and_sign_offchain_request(nonce).await;
-
-        let submit_job_request = SubmitJobRequest {
-            request: encoded_job_request,
-            signature,
-            offchain_input: Vec::new(),
-            state: Vec::new(),
-        };
-        let payload = serde_json::to_value(submit_job_request)?;
-
-        let client = reqwest::Client::new();
-        let _response = client
-            .post(format!(
-                "http://{}:{}/v1/coprocessor_node/submit_job",
-                coprocessor_gateway_ip(),
-                coprocessor_gateway_port()
-            ))
-            .json(&payload)
-            .send()
-            .await?;
-
-        println!("First job submitted with nonce 1");
-    }
-
-    Ok(())
-}
-
 async fn loadtest_submit_job(user: &mut GooseUser) -> TransactionResult {
     let start_time = Instant::now();
 
     // Get the current nonce and increment it
     let nonce = GLOBAL_NONCE.get().unwrap().fetch_add(1, Ordering::SeqCst);
 
-    let (encoded_job_request, signature) = create_and_sign_offchain_request(nonce).await;
+    let (encoded_job_request, signature) = get_offchain_request(nonce).await;
 
     let submit_job_request = SubmitJobRequest {
         request: encoded_job_request,
@@ -179,9 +80,8 @@ async fn loadtest_submit_job(user: &mut GooseUser) -> TransactionResult {
         offchain_input: Vec::new(),
         state: Vec::new(),
     };
-    let payload = serde_json::to_value(submit_job_request).expect("Valid SubmitJobRequest");
-
-    let _goose_metrics = user.post_json("/v1/coprocessor_node/submit_job", &payload).await?;
+    let _goose_metrics =
+        user.post_json("/v1/coprocessor_node/submit_job", &submit_job_request).await?;
 
     if should_wait_until_job_completed() {
         wait_until_job_completed(user, nonce).await;
@@ -197,79 +97,41 @@ async fn loadtest_get_result(user: &mut GooseUser) -> TransactionResult {
     let job_id = get_job_id(1, consumer_addr);
 
     let get_result_request = GetResultRequest { job_id: job_id.to_vec() };
-    let payload = serde_json::to_value(get_result_request).expect("Valid GetResultRequest");
-
-    let _goose_metrics = user.post_json("/v1/coprocessor_node/get_result", &payload).await?;
+    let _goose_metrics =
+        user.post_json("/v1/coprocessor_node/get_result", &get_result_request).await?;
 
     Ok(())
 }
 
-async fn create_and_sign_offchain_request(nonce: u64) -> (Vec<u8>, Vec<u8>) {
-    let consumer_addr: Address =
-        Address::parse_checksummed(consumer_addr(), None).expect("Valid address");
+/// Submit the first job to the coprocessor node so that
+/// `loadtest_get_result()` is guaranteed to get the result of
+/// this job. Only need to do this if the next nonce is 1.
+pub async fn submit_first_job() -> Result<(), Box<dyn std::error::Error>> {
+    let nonce = GLOBAL_NONCE.get().unwrap().fetch_add(1, Ordering::SeqCst);
 
-    let encoded_consumer_addr = Address::abi_encode(&consumer_addr);
-    let job_params = JobParams {
-        nonce,
-        max_cycles: max_cycles(),
-        // Need to use abi_encode_packed because the contract address
-        // should not be zero-padded
-        consumer_address: Address::abi_encode_packed(&consumer_addr)
-            .try_into()
-            .expect("Valid consumer address"),
-        onchain_input: encoded_consumer_addr.as_slice(),
-        program_id: &MOCK_CONSUMER_GUEST_ID
-            .iter()
-            .flat_map(|&x| x.to_le_bytes())
-            .collect::<Vec<u8>>(),
-        offchain_input_hash: *keccak256([]),
-        state_hash: *keccak256([]),
-    };
+    if nonce == 1 {
+        let (encoded_job_request, signature) = get_offchain_request(nonce).await;
 
-    let encoded_job_request = abi_encode_offchain_job_request(job_params);
+        let submit_job_request = SubmitJobRequest {
+            request: encoded_job_request,
+            signature,
+            offchain_input: Vec::new(),
+            state: Vec::new(),
+        };
 
-    let signers = get_signers(6);
-    let offchain_signer = signers[5].clone();
-    let signature =
-        offchain_signer.sign_message(&encoded_job_request).await.expect("Signing should work");
+        let client = reqwest::Client::new();
+        let _response = client
+            .post(format!(
+                "http://{}:{}/v1/coprocessor_node/submit_job",
+                coprocessor_gateway_ip(),
+                coprocessor_gateway_port()
+            ))
+            .json(&submit_job_request)
+            .send()
+            .await?;
 
-    (encoded_job_request, signature.as_bytes().to_vec())
-}
-
-async fn wait_until_job_completed(user: &mut GooseUser, nonce: u64) {
-    let consumer_addr: Address =
-        Address::parse_checksummed(consumer_addr(), None).expect("Valid address");
-    let job_id = get_job_id(nonce, consumer_addr);
-
-    loop {
-        match get_result_status(user, job_id).await {
-            Ok(status) => {
-                // 2 is the status code for a completed job
-                if status == 2 {
-                    break;
-                }
-            }
-            Err(e) => eprintln!("Error getting result: {:?}", e),
-        }
-        tokio::time::sleep(tokio::time::Duration::from_millis(10)).await;
+        println!("First job submitted with nonce 1");
     }
-}
 
-async fn get_result_status(
-    user: &mut GooseUser,
-    job_id: [u8; 32],
-) -> Result<i64, Box<dyn std::error::Error>> {
-    let get_result_request = GetResultRequest { job_id: job_id.to_vec() };
-    let payload = serde_json::to_value(get_result_request)?;
-
-    let response = user.post_json("/v1/coprocessor_node/get_result", &payload).await?;
-    let get_result_response: GetResultResponse = response.response?.json().await?;
-
-    match get_result_response.job_result {
-        Some(job_result) => match job_result.status {
-            Some(status) => Ok(status.status.into()),
-            None => Ok(0),
-        },
-        None => Ok(0),
-    }
+    Ok(())
 }
