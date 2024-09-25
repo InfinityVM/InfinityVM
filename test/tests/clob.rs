ea0e13cc2aa21b4ee6976b51349d04761f8465db
Zeke Mostov <zeke@turnkey.io>
2024-09-25 11:50:40 -0400

    revert changes to test timeout

--- test/tests/clob.rs
+++ test/tests/clob.rs
@@ -217,7 +217,7 @@ async fn state_job_submission_clob_consumer() {
 #[tokio::test(flavor = "multi_thread")]
 async fn clob_node_e2e() {
     async fn test(mut args: Args) {
-        let mut interval = tokio::time::interval(tokio::time::Duration::from_secs(20));
+        let mut interval = tokio::time::interval(tokio::time::Duration::from_secs(13));
         interval.tick().await; // First tick processes immediately
 
         let anvil = args.anvil;
