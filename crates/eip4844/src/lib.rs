64ee9666346b3b01ce7cbd92b486224fdca49e5b
Zeke Mostov <zeke@turnkey.io>
2024-10-22 22:16:16 -0400

    outline some test cases

--- crates/eip4844/src/lib.rs
+++ crates/eip4844/src/lib.rs
@@ -1,18 +1,15 @@
 //! Helpers for constructing eip4844 blobs.
 //!
-//! ref: https://github.com/ethereum/consensus-specs/blob/86fb82b221474cc89387fa6436806507b3849d88/specs/deneb/polynomial-commitments.md
+//! ref: <https://github.com/ethereum/consensus-specs/blob/86fb82b221474cc89387fa6436806507b3849d88/specs/deneb/polynomial-commitments.md>
 // ref: https://github.com/paradigmxyz/reth/blob/bc43613be35f316304f7ce4a2225855ac26b5923/crates/primitives/src/transaction/sidecar.rs#L254-L256
 //
 
-use alloy::{
-    consensus::{EnvKzgSettings, SidecarBuilder, SimpleCoder, },
-    eips::eip4844::{USABLE_BYTES_PER_BLOB, FIELD_ELEMENTS_PER_BLOB},
-};
+use alloy::eips::eip4844::{FIELD_ELEMENTS_PER_BLOB, USABLE_BYTES_PER_BLOB};
 
 // The first element is used as a header (which contains the length of the data, right padded). The
 // first bit of each field is element is 0.
-const SIMPLE_CODER_MAX_DATA_PER_BLOB: usize = USABLE_BYTES_PER_BLOB - 32 - FIELD_ELEMENTS_PER_BLOB as usize;
-
+const SIMPLE_CODER_MAX_DATA_PER_BLOB: usize =
+    USABLE_BYTES_PER_BLOB - 32 - FIELD_ELEMENTS_PER_BLOB as usize;
 
 #[cfg(test)]
 mod test {
@@ -35,7 +32,42 @@ mod test {
         (0..len).map(|n| n as u8 % u8::MAX).collect()
     }
 
-    
+    #[test]
+    fn usable_bytes_per_blob_creates_two_blobs() {
+        todo!()
+    }
+
+    #[test]
+    fn simple_coder_max_data_per_blob_creates_one_blob() {
+        todo!()
+    }
+
+    #[test]
+    fn simple_coder_max_data_per_blob_plus_one_creates_two_blobs() {
+        todo!()
+    }
+
+    #[test]
+    fn simple_coder_max_data_per_blob_times_six_creates_six_blobs() {
+        todo!()
+    }
+
+    #[test]
+    fn simple_coder_max_data_per_blob_times_six_creates_six_blobs() {
+        todo!()
+    }
+
+    #[test]
+    fn simple_coder_max_data_per_blob_times_six_plus_1_creates_seven_blobs() {
+        todo!()
+    }
+
+    // https://github.com/alloy-rs/alloy/blob/0361c785e37717a2610cef300cef1e6ebf6cd5ad/crates/eips/src/eip4844/mod.rs#L34
+    #[test]
+    fn field_element_with_gt_bls_modulus_is_rejected() {
+        // TODO: what endianess are the interpreted? do we add the 0 bit to the highest order bit?
+        todo!()
+    }
 
     #[test]
     fn simple_coder() {
