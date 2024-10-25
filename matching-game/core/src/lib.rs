fec7bfdc272c7dd912a71818b8bb647292252329
Maanav Khaitan <maanav@berkeley.edu>
2024-10-25 16:40:57 -0400

    added basic lib test

--- matching-game/core/src/lib.rs
+++ matching-game/core/src/lib.rs
@@ -143,3 +143,56 @@ impl<T: BorshSerialize> BorshKeccak256 for T {
         keccak256(&borsh)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        api::{CancelNumberRequest, CancelNumberResponse, MatchPair, Request, Response, SubmitNumberRequest, SubmitNumberResponse},
+        tick, MatchingGameState,
+    };
+
+    #[test]
+    fn submit_number_cancel() {
+        let matching_game_state = MatchingGameState::default();
+        let bob = [69u8; 20];
+        let alice = [42u8; 20];
+
+        let alice_submit =
+            Request::SubmitNumber(SubmitNumberRequest { address: alice, number: 42 });
+        let (resp, matching_game_state) = tick(alice_submit, matching_game_state);
+        assert_eq!(Response::SubmitNumber(SubmitNumberResponse { success: true, match_pair: None }), resp);
+        assert_eq!(*matching_game_state.get_number_to_addresses().get(&42).unwrap(), vec![alice]);
+
+        let bob_submit =
+            Request::SubmitNumber(SubmitNumberRequest { address: bob, number: 69 });
+        let (resp, matching_game_state) = tick(bob_submit, matching_game_state);
+        assert_eq!(
+            Response::SubmitNumber(SubmitNumberResponse {
+                success: true,
+                match_pair: None,
+            }),
+            resp
+        );
+        assert_eq!(*matching_game_state.get_number_to_addresses().get(&42).unwrap(), vec![alice]);
+        assert_eq!(*matching_game_state.get_number_to_addresses().get(&69).unwrap(), vec![bob]);
+
+        let alice_cancel = Request::CancelNumber(CancelNumberRequest { address: alice, number: 42 });
+        let (resp, matching_game_state) = tick(alice_cancel, matching_game_state);
+        assert_eq!(Response::CancelNumber(CancelNumberResponse { success: true }), resp);
+        assert_eq!(matching_game_state.get_number_to_addresses().get(&42).unwrap().len(), 0);
+        assert_eq!(*matching_game_state.get_number_to_addresses().get(&69).unwrap(), vec![bob]);
+
+        let alice_second_submit =
+            Request::SubmitNumber(SubmitNumberRequest { address: alice, number: 69 });
+        let (resp, matching_game_state) = tick(alice_second_submit, matching_game_state);
+        assert_eq!(
+            Response::SubmitNumber(SubmitNumberResponse {
+                success: true,
+                match_pair: Some(MatchPair { user1: bob.into(), user2: alice.into() })
+            }),
+            resp
+        );
+        assert_eq!(matching_game_state.get_number_to_addresses().get(&42).unwrap().len(), 0);
+        assert_eq!(matching_game_state.get_number_to_addresses().get(&69).unwrap().len(), 0);
+    }
+}
