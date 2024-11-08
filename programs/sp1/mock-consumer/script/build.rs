ae57b70e591f849468ac2973c29416876594abcf
mayuridev <gltocr23@gmail.com>
2024-11-08 02:16:42 -0800

    build settings

--- programs/sp1/mock-consumer/script/build.rs
+++ programs/sp1/mock-consumer/script/build.rs
@@ -1,5 +1,12 @@
 use sp1_helper::build_program_with_args;
 
 fn main() {
-    build_program_with_args("../program", Default::default())
+    build_program_with_args(
+        "../program",
+        sp1_helper::BuildArgs {
+            docker: false,
+            output_directory: "../target/elf-compilation".to_string(),
+            ..Default::default()
+        },
+    )
 }
