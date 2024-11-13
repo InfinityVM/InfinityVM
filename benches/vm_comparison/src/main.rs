ef48f2c90dff55c2e776af4409a32a995c45caa4
Dylan <58492766+mayuridev@users.noreply.github.com>
2024-11-13 04:18:08 -0800

    merged

--- benches/vm_comparison/src/main.rs
+++ benches/vm_comparison/src/main.rs
@@ -1,20 +1,20 @@
 //! VM comparison benchmark binary
-const INTENSITY_TEST_RISC0_GUEST_ELF: &[u8] = include_bytes!(
-    "../../../target/riscv-guest/riscv32im-risc0-zkvm-elf/release/intensity-test-guest"
-);
-const INTENSITY_TEST_SP1_GUEST_ELF: &[u8] = include_bytes!("../../../elf/intensity-test-sp1-guest");
 
 fn main() {
     println!("Running VM speed comparison benchmark...\n");
 
-    let intensities = [1000, 5000, 10000, 50000];
+    let risc0_elf = std::fs::read(
+        "../../../target/riscv-guest/riscv32im-risc0-zkvm-elf/release/intensity-test-guest",
+    )
+    .unwrap();
+    let sp1_elf = std::fs::read(
+        "../../../target/elf-compilation/riscv32im-succinct-zkvm-elf/release/intensity-test-sp1-guest",
+    )
+    .unwrap();
 
+    let intensities = [1000, 5000, 10000, 50000];
     println!("Testing with intensities (hash rounds): {:?}\n", intensities);
     println!("Higher ratios mean SP1 is faster relative to RISC0\n");
 
-    vm_comparison::compare_vms(
-        INTENSITY_TEST_RISC0_GUEST_ELF,
-        INTENSITY_TEST_SP1_GUEST_ELF,
-        &intensities,
-    );
+    vm_comparison::compare_vms(&risc0_elf, &sp1_elf, &intensities);
 }
