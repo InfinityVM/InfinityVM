fdca20a3f6165119c4b1890deb10610d2e0cc4c6
Maanav Khaitan <maanav@berkeley.edu>
2024-11-20 17:38:09 +0530

    use ivm zkvm

--- crates/sp1-utils/src/lib.rs
+++ crates/sp1-utils/src/lib.rs
@@ -1,5 +1,5 @@
+use ivm_zkvm::Zkvm;
 use sp1_build::{build_program_with_args, BuildArgs};
-use sp1_sdk::{HashableKey, ProverClient};
 use std::{fs, path::PathBuf};
 
 pub fn build_sp1_program(elf_name: &str, program_dir: &str, output_dir: &str) {
@@ -25,13 +25,12 @@ pub fn build_sp1_program(elf_name: &str, program_dir: &str, output_dir: &str) {
     println!("cargo:rerun-if-changed={}", elf_path.display());
 
     if let Ok(program_elf) = fs::read(&elf_path) {
-        let (_, program_id) = ProverClient::new().setup(&program_elf);
-        let program_id_bytes = program_id.hash_bytes().to_vec();
+        let program_id = ivm_zkvm::Sp1.derive_program_id(&program_elf).unwrap();
         let program_id_path = elf_path.with_extension("vkey");
 
         bincode::serialize_into(
             fs::File::create(&program_id_path).expect("Failed to create program ID file"),
-            &program_id_bytes,
+            &program_id,
         )
         .expect("Failed to serialize program ID");
 
