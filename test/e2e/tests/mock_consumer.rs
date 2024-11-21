1d79a48ef024c2b31317ed45efbda5c90d1d86c6
mayuridev <gltocr23@gmail.com>
2024-11-20 22:05:50 -0500

    timing

--- test/e2e/tests/mock_consumer.rs
+++ test/e2e/tests/mock_consumer.rs
@@ -199,7 +199,7 @@ async fn event_job_created_coprocessor_node_mock_consumer_e2e() {
         assert_eq!(job_id, expected_job_id);
 
         // Wait for the job to be processed
-        tokio::time::sleep(tokio::time::Duration::from_secs(4)).await;
+        tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
 
         let get_result_request = GetResultRequest { job_id: job_id.to_vec() };
         let get_result_response =
