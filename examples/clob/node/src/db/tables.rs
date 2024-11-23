70f04795b6083c0807316c5f2cb37c644e9ddaf4
Zeke Mostov <zeke@turnkey.io>
2024-11-22 19:21:10 -0500

    update rust version

--- examples/clob/node/src/db/tables.rs
+++ examples/clob/node/src/db/tables.rs
@@ -9,36 +9,36 @@ reth_db::tables! {
     /// 0 => global index of latest seen
     /// 1 => global index of latest fully processed
     table GlobalIndexTable {
-       type Key = u32; 
+       type Key = u32;
        type Value = u64;
     }
 
     /// Requests table, keyed by global index.
     table RequestTable {
-        type Key = u64; 
+        type Key = u64;
         type Value = RequestModel;
     }
 
     /// Responses table, keyed by global index.
     table ResponseTable {
-        type Key = u64; 
-        type Value = ResponseModel; 
+        type Key = u64;
+        type Value = ResponseModel;
     }
 
     /// ClOB State table, keyed by global index.
     table ClobStateTable {
-        type Key = u64; 
+        type Key = u64;
         type Value = ClobStateModel;
     }
 
     /// Diff table, keyed by global index.
     table DiffTable {
-        type Key = u64; 
-        type Value = VecDiffModel; 
+        type Key = u64;
+        type Value = VecDiffModel;
     }
 
-    table BlockHeightTable { 
-        type Key = u32; 
-        type Value = u64; 
+    table BlockHeightTable {
+        type Key = u32;
+        type Value = u64;
     }
 }
