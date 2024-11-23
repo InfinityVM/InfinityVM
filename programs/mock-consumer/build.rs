4ac6e8ef86d8b1bbc3bb2c11a800542b7b5a07e2
Maanav Khaitan <maanav@berkeley.edu>
2024-11-23 15:25:45 +0530

    fix: add cargo rerun for ELF builds

--- programs/mock-consumer/build.rs
+++ programs/mock-consumer/build.rs
@@ -1,5 +1,11 @@
 use ivm_sp1_utils::build_sp1_program;
 
 fn main() {
+    let current_dir = std::env::current_dir().unwrap();
+    println!("cargo:rerun-if-changed={}", current_dir.join("src/lib.rs").display());
+    println!("cargo:rerun-if-changed={}", current_dir.join("Cargo.toml").display());
+
+    println!("cargo:rerun-if-changed={}", current_dir.join("program/src/main.rs").display());
+    println!("cargo:rerun-if-changed={}", current_dir.join("program/Cargo.toml").display());
     build_sp1_program("mock-consumer-sp1-guest", "program/", "target/sp1/mock-consumer/");
 }
