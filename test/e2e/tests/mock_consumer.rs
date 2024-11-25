18da204ed1667ab0ee936c5d8aa0b0d8d954f8c5
Zeke Mostov <zeke@turnkey.io>
2024-11-25 17:34:59 -0500

    reduce timeout in  mock consumer

--- test/e2e/tests/mock_consumer.rs
+++ test/e2e/tests/mock_consumer.rs
@@ -201,7 +201,7 @@ async fn event_job_created_coprocessor_node_mock_consumer_e2e() {
         assert_eq!(job_id, expected_job_id);
 
         // Wait for the job to be processed
-        tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
+        tokio::time::sleep(tokio::time::Duration::from_secs(8)).await;
 
         let get_result_request = GetResultRequest { job_id: job_id.to_vec() };
         let get_result_response =
