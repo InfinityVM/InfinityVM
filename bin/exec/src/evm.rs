2621397208546d73ee40e78dcbaeb8e63346be10
Zeke Mostov <zeke@turnkey.io>
2024-12-19 16:01:07 -0800

    ConfigureEvmEnv compiles

--- bin/exec/src/evm.rs
+++ bin/exec/src/evm.rs
@@ -1,37 +1,16 @@
 use std::{sync::Arc};
-use reth::chainspec::{Chain, ChainSpec};
-use alloy::consensus::Header;
-use alloy::genesis::Genesis;
-use alloy::primitives::{address, Address, Bytes, U256};
-use reth::{
-    builder::{
-        components::{ExecutorBuilder, PayloadServiceBuilder},
-        BuilderContext, NodeBuilder,
-    },
-    payload::{EthBuiltPayload, EthPayloadBuilderAttributes},
-    revm::{
-        handler::register::EvmHandler,
-        inspector_handle_register,
-        precompile::{Precompile, PrecompileOutput, PrecompileSpecId},
-        primitives::{BlockEnv, CfgEnvWithHandlerCfg, Env, PrecompileResult, TxEnv},
-        ContextPrecompiles, Database, Evm, EvmBuilder, GetInspector,
-    },
-    rpc::types::engine::PayloadAttributes,
-    tasks::TaskManager,
-    transaction_pool::{PoolTransaction, TransactionPool},
-};
+use reth::chainspec::ChainSpec;
+// use alloy::consensus::Header;
+use alloy::primitives::{Address, Bytes, U256};
+use reth::revm::primitives::{BlockEnv, CfgEnvWithHandlerCfg, Env, TxEnv};
 use reth_evm_ethereum::EthEvmConfig;
 use reth_node_api::{
-    ConfigureEvm, ConfigureEvmEnv, FullNodeTypes, NextBlockEnvAttributes, NodeTypes,
-    NodeTypesWithEngine, PayloadTypes,
+    ConfigureEvmEnv, NextBlockEnvAttributes,
 };
-use reth_node_core::{args::RpcServerArgs, node_config::NodeConfig};
-use reth_node_ethereum::{
-    node::{EthereumAddOns, EthereumPayloadBuilder},
-    BasicBlockExecutorProvider, EthExecutionStrategyFactory, EthereumNode,
-};
-use reth::primitives::{EthPrimitives, TransactionSigned};
-use reth_tracing::{RethTracer, Tracer};
+use reth::primitives::TransactionSigned;
+use std::convert::Infallible;
+use reth::primitives::Header;
+
 
 /// IVM's EVM configuration
 #[derive(Debug, Clone)]
@@ -43,5 +22,43 @@ pub struct IvmEvmConfig {
 impl IvmEvmConfig {
     pub const fn new(chain_spec: Arc<ChainSpec>) -> Self {
         Self { eth: EthEvmConfig::new(chain_spec) }
+    }
+}
+
+impl ConfigureEvmEnv for IvmEvmConfig {
+    type Header = Header;
+    type Transaction = TransactionSigned;
+
+    type Error = Infallible;
+
+    fn fill_tx_env(&self, tx_env: &mut TxEnv, transaction: &TransactionSigned, sender: Address) {
+        self.eth.fill_tx_env(tx_env, transaction, sender);
+    }
+
+    fn fill_tx_env_system_contract_call(
+        &self,
+        env: &mut Env,
+        caller: Address,
+        contract: Address,
+        data: Bytes,
+    ) {
+        self.eth.fill_tx_env_system_contract_call(env, caller, contract, data);
+    }
+
+    fn fill_cfg_env(
+        &self,
+        cfg_env: &mut CfgEnvWithHandlerCfg,
+        header: &Self::Header,
+        total_difficulty: U256,
+    ) {
+        self.eth.fill_cfg_env(cfg_env, header, total_difficulty);
+    }
+
+    fn next_cfg_and_block_env(
+        &self,
+        parent: &Self::Header,
+        attributes: NextBlockEnvAttributes,
+    ) -> Result<(CfgEnvWithHandlerCfg, BlockEnv), Self::Error> {
+        self.eth.next_cfg_and_block_env(parent, attributes)
     }
 }
