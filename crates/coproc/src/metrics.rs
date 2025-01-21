0dd6d02b596f6934f85600ad25e18c89db7eb213
0x25d310b7 <193016706+0x25d310b7@users.noreply.github.com>
2025-01-21 19:46:00 +0100

    feat: Add metric for job execution times (#473)
    This PR adds a new histogram prometheus metric. I chose the buckets to
    easily cover the time ranges I'd expect from jobs (~500ms - 16s). We can
    adjust them in the future based on measurements.
    
    Example: Grafana heatmap from a gtx workload
    <img width="322" alt="Screenshot 2025-01-21 at 19 05 10"
    src="https://github.com/user-attachments/assets/a40ec8e5-2e21-4e15-b01d-8db53011140a"
    />

--- crates/coproc/src/metrics.rs
+++ crates/coproc/src/metrics.rs
@@ -1,8 +1,10 @@
 //! Prometheus metrics registry wrapper and server.
 
 use axum::{extract::State, http::StatusCode, response::IntoResponse, routing::get, Router};
-use prometheus::{self, Counter, CounterVec, Encoder, Opts, Registry, TextEncoder};
-use std::{fmt::Debug, sync::Arc};
+use prometheus::{
+    self, Counter, CounterVec, Encoder, Histogram, HistogramOpts, Opts, Registry, TextEncoder,
+};
+use std::{fmt::Debug, sync::Arc, time::Duration};
 
 /// Custom prometheus metrics
 #[derive(Debug, Clone)]
@@ -10,6 +12,7 @@ pub struct Metrics {
     job_errors: CounterVec,
     relay_errors: CounterVec,
     relayed_total: Counter,
+    job_exec_time: Histogram,
 }
 
 /// Metrics Server
@@ -23,15 +26,27 @@ impl Metrics {
     pub fn new(registry: &Registry) -> Self {
         let job_errors_opts = Opts::new("job_errors_total", "Total job processing errors");
         let relay_errors_opts = Opts::new("relay_errors_total", "Total relay errors");
+
         let job_errors = CounterVec::new(job_errors_opts, &["error_type"]).unwrap();
         let relay_errors = CounterVec::new(relay_errors_opts, &["error_type"]).unwrap();
         let relayed_total = Counter::new("relayed_total", "Total number of jobs relayed").unwrap();
+        let job_exec_time = Histogram::with_opts(
+            HistogramOpts::new("job_exec_time", "Total time for a single job to execute").buckets(
+                // From 562.5ms to 16s, polynomial
+                vec![
+                    0.5625, 1., 1.5625, 2.25, 3.0625, 4., 5.0625, 6.25, 7.5625, 9., 10.5625, 12.25,
+                    14.0625, 16.,
+                ],
+            ),
+        )
+        .unwrap();
 
         registry.register(Box::new(job_errors.clone())).unwrap();
         registry.register(Box::new(relay_errors.clone())).unwrap();
         registry.register(Box::new(relayed_total.clone())).unwrap();
+        registry.register(Box::new(job_exec_time.clone())).unwrap();
 
-        Self { job_errors, relay_errors, relayed_total }
+        Self { job_errors, relay_errors, relayed_total, job_exec_time }
     }
 
     /// Increment job errors counter
@@ -45,6 +60,10 @@ impl Metrics {
     /// Increment counter for total number of jobs relayed
     pub fn incr_relayed_total(&self) {
         self.relayed_total.inc();
+    }
+    /// Record a time measurement of a single job execution.
+    pub fn observe_job_exec_time(&self, duration: Duration) {
+        self.job_exec_time.observe(duration.as_secs_f64());
     }
 }
 
